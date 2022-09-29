9a62fb096c4fa8d3b0541d77076bb56166a13bec
crysv <ocommandblock@gmail.com>
2022-09-29 17:30:24 +0800

    commit

---
+++ src/main/java/com/example/examplemod/Ai.java
@@ -0,0 +1,12 @@
+package com.example.examplemod;
+
+import java.util.LinkedList;
+
+import net.minecraft.world.level.LevelAccessor;
+
+public class Ai {
+	public Ai(LevelAccessor level)
+    {
+        
+    }
+}
